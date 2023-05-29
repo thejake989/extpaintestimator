dc1677295c0ff8872b960c99a5e0c6c93260b041
Jake <thejakethree989@gmail.com>
2023-05-28 19:16:11 -0500

    fixed the estimating calculator, added clear button

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+//LOGIN FUNCTION
+
 document.addEventListener("DOMContentLoaded", () => {
   // Add event listeners to the login and signup forms
   const loginForm = document.getElementById("loginForm");
@@ -59,6 +61,7 @@ function handleLogin(event) {
     });
 }
 
+// SIGN UP FUNCTION
 // Handle signup form submission
 function handleSignup(event) {
   event.preventDefault();
@@ -101,6 +104,7 @@ function handleSignup(event) {
   }
 }
 
+//LOGOUT BUTTON FUNCTION
 // Handle logout button click
 function handleLogout() {
   // Make a POST request to the logout endpoint
@@ -137,3 +141,71 @@ function displayMessage(message) {
     output.textContent = message;
   }
 }
+
+//PAINT ESTIMATING FUNCTION
+function calculatePaintEstimate() {
+  // Estimation Constants
+  const coverageRate = 350; // Typical paint coverage in sq ft per gallon.
+
+  // DOM Elements
+  const sqftEl = document.getElementById("sqft");
+  const coatsEl = document.getElementById("coats");
+  const ppgEl = document.getElementById("ppg");
+  const laborEl = document.getElementById("labor");
+  const outputEl = document.getElementById("output");
+
+  const sqft = parseInt(sqftEl.value);
+  const coats = parseInt(coatsEl.value);
+  const ppg = parseFloat(ppgEl.value);
+  const labor = parseFloat(laborEl.value);
+
+  // Calculate paint needed (gallons)
+  const paintNeeded = (sqft / coverageRate) * coats;
+
+  // Calculate costs
+  const costOfPaint = paintNeeded * ppg;
+  const laborCost = sqft * labor;
+  const totalCost = costOfPaint + laborCost;
+
+  // Display output
+  outputEl.innerHTML = `
+      <h3 class="text-xl font-semibold text-gray-900">Estimation:</h3>
+      <p>Paint needed: ${paintNeeded.toFixed(2)} gallons</p>
+      <p>Cost of paint: $${costOfPaint.toFixed(2)}</p>
+      <p>Labor cost: $${laborCost.toFixed(2)}</p>
+      <p>Total cost: $${totalCost.toFixed(2)}</p>
+  `;
+}
+
+// Add event listener to the Calculate button
+document.addEventListener("DOMContentLoaded", function () {
+  const calculateButton = document.getElementById("calculate");
+  if (calculateButton) {
+    calculateButton.addEventListener("click", calculatePaintEstimate);
+  }
+});
+
+// Clear form function
+function clearForm() {
+  // DOM Elements
+  const sqftEl = document.getElementById("sqft");
+  const coatsEl = document.getElementById("coats");
+  const ppgEl = document.getElementById("ppg");
+  const laborEl = document.getElementById("labor");
+  const outputEl = document.getElementById("output");
+
+  // Clearing the values
+  sqftEl.value = "";
+  coatsEl.value = "";
+  ppgEl.value = "";
+  laborEl.value = "";
+  outputEl.innerHTML = "";
+}
+
+// Add event listener to the Clear button
+document.addEventListener("DOMContentLoaded", function () {
+  const clearButton = document.getElementById("clear");
+  if (clearButton) {
+    clearButton.addEventListener("click", clearForm);
+  }
+});
